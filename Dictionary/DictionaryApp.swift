96d9a3369ef5d36e597c976a76157e3b95a493b3
Vladimir Fibe <motiw@icloud.com>
2023-10-15 08:27:19 +0600

    Initial Commit

---
+++ Dictionary/DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryApp.swift
+//  Dictionary
+//
+//  Created by Vladimir Fibe on 15.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
